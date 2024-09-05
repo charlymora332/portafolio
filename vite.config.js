f1f131cfcc286fdae17857164380494452db6eae
Carlos Mora <charlymora332@gmail.com>
2024-09-05 16:33:14 -0500

    segundo  commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://charlymora332.github.io/portafolio/'
+  base: 'https://charlymora332.github.io/portafolio'
 })
