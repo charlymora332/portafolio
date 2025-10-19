92ed5f4cc6ee759ae091d1e4909a79c177ba7887
Carlos Mora <charlymora332@gmail.com>
2025-10-19 16:50:08 -0500

    Updates

---
+++ img/proyectos/imagenesProyectos.js
@@ -0,0 +1,26 @@
+import conectaConmigo from './conectaConmigo.png';
+import miasflorist from './miasflorist.png';
+import snap from './Snap.png';
+import TopNBA from './TopNBA.png';
+
+export default [
+  {
+    src: conectaConmigo,
+    alt: 'Imagen del proyecto Conecta Conmigo',
+  },
+  {
+    src: miasflorist,
+    alt: 'Imagen del proyecto Mias Florist',
+  },
+  {
+    src: snap,
+    alt: 'Imagen del proyecto Snap',
+  },
+  {
+    src: TopNBA,
+    alt: 'Imagen del proyecto Top NBA',
+  },
+];
+
+
+
